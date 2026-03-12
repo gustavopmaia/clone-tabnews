de2610861e3772a580f523f978b9fabade5afbc6
Gustavo Maia <gustavopmaia@yahoo.com.br>
2026-03-12 17:13:15 -0300

    add em construcao paragraph

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Gustavo Maia - NyxDev</h1>;
+  return (
+    <>
+      <h1>Gustavo Maia - NyxDev</h1>
+      <p>👷‍♂️ - Página em construção</p>
+    </>
+  );
 }
 
 export default Home;
